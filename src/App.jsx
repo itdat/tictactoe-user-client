2a153bbc4b2dcc6e6aa830dbfe390ec45ac17c0e
mandythucuyen <jacksonthucuyen@gmail.com>
2020-12-08 10:06:15 +0700

    Add Online Users and Join Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+
+import {Join} from './components'
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/login" component={Join} />
         <Route exact path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
