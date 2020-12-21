c152a80aaa3bba5a08f2b2b758486fbc0eef67df
Van Huy <1712056@student.hcmus.edu.vn>
2020-12-21 22:32:04 +0700

    Fix UI history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -10,13 +10,19 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Image from "material-ui-image";
+import Rating from "@material-ui/lab/Rating";
+import Grid from "@material-ui/core/Grid";
+import RoomSearchHeader from "../components/PlayGame/RoomSearchHeader";
+import Typography from "@material-ui/core/Typography";
+import Trophy from "../images/trophy.svg";
+import Defeat from "../images/loser.jpg";
 
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: "#004c9e",
     color: theme.palette.common.white,
     fontSize: 20,
   },
@@ -32,7 +38,15 @@ const useStyles = makeStyles({
     minWidth: 700,
   },
 });
-
+const loadData = (id, result, room, level, competitor, date) => {
+  return { id, result, room, level, competitor, date };
+};
+// const rows = [
+//   loadData(1, true, "solo", 5, "nvh", "22/12/2020"),
+//   loadData(2, false, "play", 4, "ntd", "30/12/2020"),
+//   loadData(3, false, "hard", 3, "ntd", "30/12/2020"),
+//   loadData(4, true, "fun", 2, "ntd", "30/12/2020"),
+// ];
 const History = () => {
   const classes = useStyles();
   const [rows, setRows] = useState([]);
@@ -43,48 +57,75 @@ const History = () => {
   const handleRowClick = (e) => {
     history.push("/room");
   };
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
     const res = await axios(`http://localhost:4000/users/history/${name}`);
     console.log(res.data);
     setRows([...res.data]);
   }, [rows.id]);
-
   return (
     <OnlineListWrapper>
       <h1>History</h1>
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+        style={{ marginTop: "25px", marginBottom: "25px" }}
+      >
+        <RoomSearchHeader />
+      </Grid>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
             <TableRow>
               <StyledTableCell></StyledTableCell>
               <StyledTableCell>Result</StyledTableCell>
+              <StyledTableCell>Room</StyledTableCell>
+              <StyledTableCell>Level</StyledTableCell>
               <StyledTableCell>Competitor</StyledTableCell>
               <StyledTableCell>Date</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <StyledTableRow
-                key={row.id}
-                hover="true"
-                onClick={handleRowClick}
-                style={{
-                  background:
-                    row.result === false
-                      ? "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-                      : "linear-gradient(to left, #14eec8, #00e2db, #00d4e8, #00c5ed, #12b5eb)",
-                }}
-              >
-                <StyledTableCell style={{ padding: 0 }}>
+              <StyledTableRow key={row.id} hover onClick={handleRowClick}>
+                <StyledTableCell style={{ padding: 5 }}>
                   {row.result === true ? (
-                    <Image src="https://img.icons8.com/emoji/48/000000/trophy-emoji.png" />
+                    <Image src={Trophy} />
                   ) : (
-                    ""
+                    <Image src={Defeat} />
                   )}
                 </StyledTableCell>
                 <StyledTableCell>
-                  {row.result === true ? "Thắng" : "Thua"}
+                  {row.result === true ? (
+                    <Typography
+                      style={{ color: "#64b5f6", fontWeight: "bold" }}
+                    >
+                      Victory
+                    </Typography>
+                  ) : (
+                    <Typography
+                      style={{ color: "#f44336", fontWeight: "bold" }}
+                    >
+                      Defeat
+                    </Typography>
+                  )}
+                </StyledTableCell>
+                <StyledTableCell>{row.room}</StyledTableCell>
+                <StyledTableCell>
+                  {row.level === 1 ? (
+                    <Rating name="level" value={1} readOnly size="small" />
+                  ) : row.level === 2 ? (
+                    <Rating name="level" value={2} readOnly size="small" />
+                  ) : row.level === 3 ? (
+                    <Rating name="level" value={3} readOnly size="small" />
+                  ) : row.level === 4 ? (
+                    <Rating name="level" value={4} readOnly size="small" />
+                  ) : (
+                    <Rating name="level" value={5} readOnly size="small" />
+                  )}
                 </StyledTableCell>
                 <StyledTableCell>{row.competitor}</StyledTableCell>
                 <StyledTableCell>{row.date}</StyledTableCell>
